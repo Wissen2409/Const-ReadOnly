3ba74363c349592cee4002e196e4321881917cb2
emrahelis <emrahelis@gmail.com>
2024-10-23 19:33:18 +0300

    dkfdkkffd

--- Const.cs
+++ Const.cs
@@ -1,4 +1,52 @@
 
-public class Sabit{
-    public const string Username="wissenbesiktaş";
+public class Sabit
+{
+
+
+    // Const değeri : bir kere başlangıç değeri aldığı zaman
+    // tekrardan değer verilemez bir tiptir.!!!    
+    public const string Username = "wissenbesiktaş";
+
+
+    // bir conts değişken tanımlıyorsanız, başlangıç değeri vermek zorundasınız, Eğer vermezseniz derleme zamanı hatası alırsınız
+    // Conts değişkenlere, derleme aşamasında değer vermemiz gerekir.!!
+    // const değişkene değer vermedik ve hata aldık!!
+    //public const string Password;
+
+
+    //ReadOnly keyword'ü!!
+     
+
+
+    //ReadOnly değişken tanımlama aşağıdaki gibidir.!!!
+    // Readonly adından da anlaşılabileceği gibi, bir sabit değerdir ve üstüne yazma işlemi gerçekleşmez
+    //sadece okuma işlemi yapabileceğinizden ötürü bir sabittir.!!
+
+    // Conts ile arasındaki en önemli fark, conts derleme aşamasında değer verilmesini zorunlu tutarken, 
+    //read only değişkene çalışma zamanında da değer verilebilir!!
+
+    // aşağıda değer vermeden oluşturulan bir readonly değişkenini görüyorsunuz!!!
+    public Sabit(string firmaAdi)
+    {
+        // readonly bir sabite nesne üzerinden değer atanamaz!!
+        // nesne kullanıcısı tarafından değer atanmak istenirse, ctor kullanılarak değer ataması gerçekletirilebilir!!
+        this.FirmaAdi = firmaAdi;
+    }
+        public readonly string FirmaAdi;
+
+    //public string Password ="1010";
+
+
+
+    // 500
+    // döviz kodu : 848 : Dolar 
+    // 500 Tl çek 
+    // kargoyu çık
+    /*  if(eÖdeme ok ise){
+
+
+
+
+    }*/
+
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Const keyword'ü bir değişkenin değerini sabitlemek için kullanılmaktadır
+// Const keyword'ü ile tanımlanmış olan bir değişken, başlangıç değeri verildikte nsonra, kullanılan yerde bu değeri
+// değiştiremezsiniz
+// Çok kullanılan Math.Pi değerinin tipi de conts'dur diyebiliriz
+//Sabit.Username="Merhaba";
+
+// Math.Pı bir const de 
+
+
+
+//ReadOnly değişkene değer verelim !! 
+
+Sabit s = new Sabit("Wissen");
+
+//Read only değişkene nesne üzerinden değer atayamadık.!!!
+//s.FirmaAdi="sedwsdsx";
+
+// Conts ve Readonly sabitleri arasındaki farklar!!!
+// conts tanımlandığı anda değer verilmek zorundadir. Readonly de vermesenizde olur
+// conts değeri dışarıdan alınmaz, readonnly de ctor aracılığı ile değer alınabilir!!
+
+// Conts bir sınıfın bir elemanıdır.!! ReadOnly nesnenin bir elemanıdır!! Not : Static konusunda konuşulacak!!
